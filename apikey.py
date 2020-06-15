53c64b6d5afa89d00469998e18da6fbee2d61c8f
abhishek7071 <38905469+abhishek7071@users.noreply.github.com>
2020-06-15 12:51:28 +0530

    Update apikey.py

--- apikey.py
+++ apikey.py
@@ -1,4 +1,4 @@
-SENDGRID_API_KEY=['SG.NOjkOvZUR1-hsop8YOgVtg.0twG0cjW
-']
-TO_EMAIL = 'mail@examples.com'
-FROM_EMAIL = 'mail2@examples.com'
+SENDGRID_API_KEY=['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']
+
+TO_EMAIL = 'narendra@yopmail.com'
+FROM_EMAIL = 'Abhishek7071631646@gmail.com'
